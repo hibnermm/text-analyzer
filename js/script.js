850b5a526dc173416b279eebc85d5f08be771937
J.J. Hibner <jjmhibner@gmail.com>
2023-06-25 11:07:39 -0700

    wip bold function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 // test#1
 function wordCounter(word) {
   const wordsArray = word.split(" ").length;
@@ -184,11 +185,62 @@ console.log(uniqueCounterExcerpt("yo", "yo? holler. what, yup yo! Yo"));
 
 //test 13
 function wordOmit(words, excerpt){
-
+  if words 
 }
  
-//slow down -> don't start here...
+wordOmit("zoinks", "zoinks");
+
+
+/*slow down -> don't start here...
 words = ["zoinks", "mUppetteer", "biffaroni", "loopdaloP"]
 excerpt = "Lorem ipsum zoinks dolor sit amet consectetur biffaroni adipisicing elit, Zoinks. Praesentium aliquid, muppetteer, pariatur modi veritatis maiores asperiores biffaroni omnis odit molestiae aut doloribus loopdalop."
 
 wordOmit(words, excerpt);
+*/
+
+
+
+//test 14
+
+//example 1
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+
+// UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
